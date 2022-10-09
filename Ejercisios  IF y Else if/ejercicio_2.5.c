763f8829155afb8e2374d7f899c3cf04bd7292f0
Jonatan <Jonatanmatosperez46@gmail.com>
2022-10-09 00:18:29 -0400

    ejercicio 2.5

--- Ejercisios  IF y Else if/ejercicio_2.5.c
+++ Ejercisios  IF y Else if/ejercicio_2.5.c
@@ -1,9 +1,34 @@
 # include <stdio.h>
 # include <math.h>
 
+/*El programa optiene el resultado de una funcion*/
+
 int main ()
 {
-    int ;
+    int op, t; /*variables de tipo entero*/
+    float res; /*varianle de tipo real*/
+    printf("ingrese la opcion del calculo y el valor entero");
+    scanf("%d %d", &op, &t);
+
+    switch (op)
+    {
+    case 1: res =  t / 5;
+        /* code */
+        break;
+
+    case 2: res = pow(t/t);
+    /*la funcion pow esta definida en la biblioteca math.h*/
+    break;
+
+    case 3:
+    case 4: res = 6 * t/2;
+    break;
+
+    default: res = 1;
+        break;
+    }
+
+    
     
     return 0;
 
