c6a72c37c55c9981bba1156884fa96b04375e4ce
Jonatan <Jonatanmatosperez46@gmail.com>
2022-10-18 00:22:07 -0400

    nuevo formato

---
+++ Capitulo 2/La estructura selectiva simple if 1/ejercicio_2.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*incremento de precio.
+El programa recibe como dato el precio del prodcto importado,
+este incrementa un 11% al precio recibido si este es menor a 1500 */
+
+int main()
+{
+    float pre; /* la variable pre, representa el precio del producto. */
+    float npr; /* la variable npr, almacena el nuevo precio del producto.*/
+
+    printf("ingrese el precio del producto: "); /*pide que se ingrese el precio al usuario*/
+    scanf("%f",&pre);/* almacena el precio suministrado por el usuario en la direccion de memoria asignada a la variable (pre) */
+
+    if (pre < 1500) /* hace una evaluacion para saber si hay que calcular el aumento*/
+    {
+        npr = pre * 1.11; /* calcula el aumento del 11% */
+        printf("\nel nuevo precio del producto es: %7.2f", npr); /* imprime en consola el precio del producto con el aumento del 11% */
+    }
+
+    return 0;
+}
