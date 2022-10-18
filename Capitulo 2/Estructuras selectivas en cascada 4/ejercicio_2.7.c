1bc33b18061a5b2f59781b0597eacb0cae1c8792
Jonatan <Jonatanmatosperez46@gmail.com>
2022-10-18 02:14:04 -0400

    nuevo codigo

--- Capitulo 2/Estructuras selectivas en cascada 4/ejercicio_2.7.c
+++ Capitulo 2/Estructuras selectivas en cascada 4/ejercicio_2.7.c
@@ -16,13 +16,38 @@ int main()
         {
             if (s>r)
             {
-                printf("\n\n El orden es: P, R y S: %8.2f %8.2f %8.2f", p, s, r);
+                printf("\n\n El orden es: P, S y R: %8.2f %8.2f %8.2f", p, s, r);
             }
             else
             {
-                printf("\n\n El orden es P, S y R: %8.2f %8.2f %8.3f", p, s, r);
+                printf("\n\n El orden es P, R y S: %8.2f %8.2f %8.3f", p, r, s);
             }
             
+        }
+
+        else
+        {
+            printf("\n\n El orden es R, P y S: %8.2f %8.2f %8.2f", r, p, s);
+        }
+    }
+
+    else
+    {
+        if (s>r)
+        {
+            if (r>p)
+            {
+                printf("\n\n El orden es S, R y P: %8.2f %8.2f %8.2f", s, r, p);
+            }
+            else
+            {
+                printf("\n\n El orden es S, P y R: %8.2f %8.2f %8.2f", s, p, r);
+            }
+        }
+
+        else
+        {
+            printf("\n\n El orden es R, S y P: %8.2f %8.2f %8.2f", r, s, p);
         }
     }
     
