cb3e58afc5035ba551e150d11016d6707056b797
Jonatan <Jonatanmatosperez46@gmail.com>
2022-10-24 00:44:37 -0400

    ejercicio_3.6

---
+++ Capitulo 3/la estructura repetitiva do while 3/ejercicio_3.6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;//variable de tipo entero
+    float sal, pro, nom = 0;// variable de tipo real.
+    printf("ingrese el salario del profesor:\t");
+    // se necesita el salario de almenos un profesor para que no de error.
+    scanf("%f", &sal);
+
+    do
+    {
+        nom = nom + sal;
+        i++;
+        printf("ingrese el salario del profesor -0 para terminar- :\t");
+        scanf("%f", &sal);
+
+    } while (sal);
+
+    pro = nom / i;
+    printf("\nNomina: %.2f \t Promedio de salarios: %.2f", nom, pro);    
+
+    return 0;
+}
